51682743a195f212c5e54ea6360562421519b831
Linguardium <Linguardium@hobbyist.dev>
2020-07-22 08:05:13 -0700

    actually fix bees
    actually add bee mixin to the config

--- src/main/java/mod/linguardium/animalspawns/mixin/BeeEntityMixin.java
+++ src/main/java/mod/linguardium/animalspawns/mixin/BeeEntityMixin.java
@@ -7,6 +7,7 @@ import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BeeEntity.class)
@@ -16,10 +17,9 @@ public abstract class BeeEntityMixin extends AnimalEntity {
         super(entityType, world);
     }
 
-    // Bees dont despawn due to rarity
     @Override
-    public boolean canImmediatelyDespawn(double distanceSquared) {
-        return false;
+    public boolean cannotDespawn() {
+        return true;
     }
 
 }
